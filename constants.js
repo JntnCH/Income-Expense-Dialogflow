73ee876ce50766237c13dedd50fa4db4fad41296
JntnCH <196344663+JntnCH@users.noreply.github.com>
2025-05-22 04:16:38 +0700

    update constants.js

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-// ใส่ SHEET_ID ฟังก์ชั่นนี้
+// ใส่ SHEET_ID ของคุณ ทึ่ฟังก์ชั่นนี้
 function setSheetId() {
   PropertiesService.getScriptProperties.setProperty('SHEET_ID', 'YOUR_SHEET_ID_HERE');
   return "✅ ตั้งค่า SHEET เรียบร้อยแล้ว";
